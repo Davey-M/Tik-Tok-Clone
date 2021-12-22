9373a28771f0a934507ceba77e8c79435ba4febc
Davey-M <davey.meuer@gmail.com>
2021-12-21 19:40:08 -0600

    added javascript for putting posts on front page

--- public/script.js
+++ public/script.js
@@ -1,39 +1,81 @@
-class Tag
+class Post
 {
-    constructor(name, eventhandler, event)
+    constructor(username, user_nickname, content, tags, likes, shares, comments)
     {
-        this.name = name;
+        this.username = username;
+        this.user_nickname = user_nickname;
+        this.content = content;
+        this.tags = tags;
+        this.likes = likes ?? 0;
+        this.shares = shares ?? 0;
+        this.comments = comments ?? 0;
 
-        this.class = name;
-
-        this.DOM = document.getElementsByClassName(this.class);
-
-        for (let i = 0; i < this.DOM.length; i++)
-        {
-            this.DOM[i].addEventListener(this.eventhandler, this.event);
+        this.tagString = () => {
+            let data = '';
+            for (let tag of this.tags)
+            {
+                data += `<span class="tag" >#${tag}</span>`
+            }
+            return data;
         }
-    }
 
-    insert(parent)
-    {
-        parent.insertAdjacentHTML('beforeend', `<${this.name} class="${this.class}" ></${this.name}>`)
-
-        this.DOM = document.getElementsByClassName(this.class);
+        this.html = `
+        <div class="card">
+            <div class="pic"></div>
+            <div class="post">
+                <div class="row">
+                    <b>${this.username}</b>
+                    <p class="nickname">${this.user_nickname}</p>
+                </div>
+                <div class="row">
+                    <p class="hash">${this.tagString()}</p>
+                </div>
+                <div class="row">
+                    <div class="post_content">
+                        <p>
+                            ${this.content}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="reactions">
+                <div class="react_holder">
+                    <div class="reaction">
+                        <div class="circle"></div>
+                        <b>${this.likes}</b>
+                    </div>
+                    <div class="reaction">
+                        <div class="circle"></div>
+                        <b>${this.comments}</b>
+                    </div>
+                    <div class="reaction">
+                        <div class="circle"></div>
+                        <b>${this.shares}</b>
+                    </div>    
+                </div>
+            </div>
+            <div class="fourth">
+                <b class="follow_button">Follow</b>
+            </div>
+        </div>
+        `
     }
 }
 
-class Card
+const content = document.getElementById('page_content');
+
+function pushPost(post)
 {
-    constructor()
-    {
-        console.log(this);
-    }
+    content.insertAdjacentHTML('beforeend', post.html);
 }
 
-class Account
+for (let i = 0; i < 10; i++)
 {
-    constructor()
-    {
-        console.log(this);
-    }
+    (async () => {
+        let words = await fetch('https://random-word-api.herokuapp.com/word?number=4&swear=0');
+        let data = await words.json();
+        
+        let thisPost = new Post(data[0], data[1], data[2], [data[3]], Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000));
+        pushPost(thisPost);
+    })()
 }
